474d7e5105160e493de5ff994a16d6ef110675cf
eJme1992 <edwin.jme@gmail.com>
2018-08-26 13:34:57 -0400

    Login completo vistas etc

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['login'] = 'Login';
+$route['admin'] = 'Admin';
 $route['panel'] = 'Panel';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
