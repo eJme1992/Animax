b17fb2d65a668d14571415fed65b53858e61f6d4
Sofia <sofiasinger19@gmail.com>
2018-09-03 11:04:37 -0400

    changes

--- application/views/website/header.php
+++ application/views/website/header.php
@@ -6,41 +6,68 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="<?=base_url();?>plantilla/website/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">    
+    <title>Document</title>
 </head>
 <body>
-    <nav id="barra-principal">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-1">
-                    <div class="logo">
+  <header>
+<nav class="navbar navbar-expand-sm bg-light">
+    <div class="container-fluid row">
+    
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon" ><i class="fas fa-bars" style="color:#000;"></i></span>
+</button>
+  <div class="col-9">
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <ul class="navbar-nav">
+     <li class="nav-item logo">
                         <a href=""><img src="<?=base_url();?>plantilla/website/img/logo.png"></a>
-                    </div>
-                </div>  
-                <div class="col-md-2">
-                    <div class="menu">  
-                            <h3>
-                            <a href="#">Inicio</a>
-                            <a href="#">Mas</a>
-                            </h3>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    <div class="buscador">
-                    <form>
-                        <input type="text" placeholder="">
-                        <i class="fas fa-search" style="color:#fff !important;"></i>
-                    </form>
-                    </div>
-                </div>
-                 <div class="col-md-4">
-                   <div class="menu2 float-right">
-                   <h3><a href="#">Series</a>
-                   <a href="#">pelis</a>
-                   <i class="fas fa-user"></i>
-                   <i class="fas fa-upload"></i></h3>
-                </div>
-                </div>
-            </div>
-        </div>
-    </nav>        
+                   </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Maestro</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Audiofrecuencia</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Game Center</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Vivo</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Los miembros compran</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">BML</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Mengzhan</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Mengzhan</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"><i class="fas fa-mobile fa-lg"></i>Descargar App</a>
+    </li>
+  </ul>
+</div>
+</div>
+    <div class="col-3">
+      <button onclick="oculta()" class="font-s"><i class="fas fa-search"></i></button>
+      <div id="buscar">
+        <form class="form-inline" action="/action_page.php">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-success" type="submit">Search</button>
+        </form>
+    </div>
+    </div>
+</div>
+</nav>
+</header>
+ 
+<script type="text/javascript">
+  function oculta(){
+  document.getElementById("buscar").style.display:'block';
+}
+</script>
