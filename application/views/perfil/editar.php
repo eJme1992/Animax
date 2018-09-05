8a9bff62cdf89b0e78d4f9786f4580529f28ac88
Sofia <sofiasinger19@gmail.com>
2018-09-05 16:20:53 -0400

    cambios en editar

--- application/views/perfil/editar.php
+++ application/views/perfil/editar.php
@@ -13,6 +13,18 @@
 			<div class="col-md-12"><button type="button" class="btn btn-cargar mx-auto d-block" id="mostrarm">
   Cambiar Imagen
 </button></div>
+
+						<div class="col-md-12">
+							<h3 class="subt-editar">Datos de Seguridad <i class="fas fa-user-lock fa-2x"></i></h3>
+								<div class="form-group">
+				  <label for="pwd">Nueva Contraseña</label>
+				  <input type="password" class="form-control" id="change-pass" name="change-pass" >
+				</div><div class="form-group">
+				  <label for="pwd">Confirmar Contraseña</label>
+				  <input type="password" class="form-control" id="confirm-pass" name="confirm-pass">
+				</div>
+						</div>
+
 				<!-- The Modal -->
 			<div class="modal" id="miimagen">
 			  <div class="modal-dialog">
@@ -33,6 +45,7 @@
 			</div>
 		</div>
 		<div class="col-md-7">
+			<h3 class="subt-editar">Datos Personales <i class="fas fa-users-cog fa-2x"></i></h3>
 			<form id="Form-cambio">
 				 <div class="form-group">
 				  	<label for="usr">Nombre:</label>
@@ -44,7 +57,7 @@
 				</div>
 				<div class="form-group">
 				  <label for="pwd">Email:</label>
-				  <input type="Email" class="form-control" id="mail" name="mail" placeholder="your@email.com">
+				  <input type="Email" class="form-control" id="mail" name="mail" placeholder="Example: your@email.com">
 				</div>
 					<div class="form-group">
 				  <label for="pwd">Sexo:</label>
@@ -61,6 +74,7 @@
 				</div>
 				<button  type="submit" class="btn btn-cargar mx-auto d-block">Enviar</button>
 				</form> 
+				<div class="col-md-12" id="resultado" style="margin-top:15px;"></div>
 			</div>
 		</div>
 	</div>
@@ -73,6 +87,87 @@
         $("#miimagen").hide();
     });
 });
+
+jQuery(document).ready(function() {
+      jQuery("#Form-cambio").submit(function(event) {
+         event.preventDefault();
+         
+         var msj = '1';
+         //validaciones con js
+         
+        if (msj === "1") {
+         var formData = new FormData(jQuery('#Form-cambio') [0]);
+         jQuery.ajax({
+           url: '<?=base_url();?>user/editar_usuario',
+           type: 'POST',
+           contentType: false,
+           processData: false,
+           dataType: 'json',
+           data: formData,
+           beforeSend: function() {
+             $("#resultado").html('<div class="alert alert-success">Procesando...</div>');
+           }
+         })
+         .done(function(data, textStatus, jqXHR) {
+            var getData = jqXHR.responseJSON; // dejar esta linea
+           if(data.status=='ok'){
+            $("#resultado").html('<div class="alert alert-success">'+data.code+'</div>');
+             window.location.href ='<?=base_url();?>/panel/user';
+           }else{
+           $("#resultado").html('<div class="alert alert-danger"><strong>ERROR!</strong>'+data.error+'</div>');
+           }
+         })
+         .fail(function(jqXHR, textStatus, errorThrown) {
+           var getErr = jqXHR.responseText;
+           
+           console.log(getErr);
+   
+         })
+          // Fin de ajax
+          } else {
+              swal("¡Error! ", msj, "error");
+          }
+      });
+      jQuery("#change-img").submit(function(event) {
+         event.preventDefault();
+         var msj = '1';
+        //validaciones con js
+        
+       if (msj === "1") {
+        var formData = new FormData(jQuery('#change-img') [0]);
+        jQuery.ajax({
+          url: '<?=base_url();?>user/editar_img',
+          type: 'POST',
+          contentType: false,
+          processData: false,
+          dataType: 'json',
+          data: formData,
+          beforeSend: function() {
+            $("#resultado2").html('<div class="alert alert-success">Procesando...!</div>');
+          }
+        })
+        .done(function(data, textStatus, jqXHR) {
+           var getData = jqXHR.responseJSON; // dejar esta linea
+          if(data.status=='ok'){
+           $("#resultado2").html('<div class="alert alert-success">'+data.code+'</div>');
+           window.location.href ='<?=base_url();?>panel/usuario/<?=$user->id;?>';
+          }else{
+          $("#resultado2").html('<div class="alert alert-danger"><strong>ERROR!</strong>'+data.error+'</div>');
+          }
+        })
+              .fail(function(jqXHR, textStatus, errorThrown) {
+                var getErr = jqXHR.responseText;
+                
+                console.log(getErr);
+        
+              })
+         // Fin de ajax
+         } else {
+             swal("¡Error! ", msj, "error");
+         }
+       });
+
+
 
 </script>
 </section>
