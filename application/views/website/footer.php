c9d2d616c7b743b39d97a154a8c41d24663ac690
eJme1992 <edwin.jme@gmail.com>
2018-09-07 18:53:18 -0400

    diverson cambios de maquetacion del home

--- application/views/website/footer.php
+++ application/views/website/footer.php
@@ -1,107 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="<?=base_url();?>plantilla/website/css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">  
-    <title>Document</title>
-</head>
-<body>
-
-</body>
 <footer>
     <section id="tope">
         <div class="container">
             <div class="row">
-                <div class="col-sm-2">
-                    <div class="bloque-1">
-                    <h3><a href="">lorem</a></h3>
-                    <h4><a href="#">Ipsum</a> <a href="#">lorem</a></h4>
-                    <h4><a href="#">lorem</a></h4>
-                    </div>
+                <div class="col-md-4">
+                    <h3 class="titulo-fo">Acerca de nosotros</h3>
+                    <p class="sub-fo"> Lorem ipsum dolor sit amet consectetur adipiscing elit dapibus erat, parturient est quam cum himenaeos potenti a viverra, ultrices arcu volutpat donec turpis justo vehicula fusce.<br> Vehicula natoque donec auctor quisque odio ante posuere, ligula id scelerisque montes augue turpis, laoreet placerat habitant libero conubia eleifend. </p>
                 </div>
-                <div class="col-sm-2">
-                    <div class="bloque-2">
-                        <h3><a href="">lorem</a></h3>
-                        <h4><a href="">lorem</a> <a href="#">lorem</a></h4>
-                        <h4><a href="#">Ipsum</a> <a href="#">lorem</a></h4>
-                        <h4><a href="#">lorem</a></h4> 
-                    </div>      
-                </div> 
-                <div class="col-sm-2">
-                    <div class="bloque-3"> 
-                        <h3><a href="">lorem</a></h3>
-                        <h4><a href="#">Ipsum</a></h4>
-                        <h4><a href="#">lorem</a></h4>
-                    </div>
-                </div>   
-                <div class="col-sm-2">
-                    <div class="bloque-4"> 
-                        <h3><a href="">lorem</a></h3>
-                        <h4><a href="#">Ipsum</a></h4>
-                    </div>
-                </div> 
-                <div class="col-sm-2">
-                        <div class="bloque-5">
-                            <h3><a href="">lorem</a></h3>
-                            <h4><a href="">lorem</a>   <a href="#">lorem</a>   <a href="#">lorem</a></h4>
-                            <h4><a href="#">Ipsum</a> <a href="#">lorem</a></h4>
-                        </div>      
-                    </div> 
-                <div class="col-sm-2">
-                    <div class="bloque-6">
-                        <img src="<?=base_url();?>plantilla/website/img/31.png">
-                    </div>
+                <div class="col-md-4">
+                     <h3 class="titulo-fo">Contactanos</h3>
+                     <ul class="contact">
+                         <li><a href="#"><i class="fas fa-phone"></i>(777)777 7777 </a></li>
+                         <li><a href="#"> <i class="fas fa-map-marker-alt"></i>Lorem ipsum dolor sit amet consectetur adipiscing elit dapibus</a></li>
+                         <li><a href="#"><i class="fas fa-envelope"></i> noreply@yourdomain.com</a></li>
+                         
+                     </ul>
                 </div>
-            </div>
-        </div>
-        </section>
-        <section id="medio">
-                <div class="container">
-                        <div class="row">
-                            <div class="col-sm-2">
-                                <div class="bloque-7">
-                                <h3><a href="">lorem</a></h3>
-                                <h4><a href="#">Ipsum</a> <a href="#">lorem</a></h4>
-                                <h4><a href="#">lorem</a></h4>
-                                </div>
-                            </div>
-                            <div class="col-sm-2">
-                                <div class="bloque-8">
-                                    <h3><a href="">lorem</a></h3>
-                                    <h4><a href="">lorem</a> <a href="#">lorem</a></h4>
-                                    <h4><a href="#">Ipsum</a> <a href="#">lorem</a></h4>
-                                    <h4><a href="#">lorem</a></h4> 
-                                </div>      
-                            </div> 
-                            <div class="col-sm-2">
-                                <div class="bloque-9"> 
-                                    <h3><a href="">lorem</a></h3>
-                                    <h4><a href="#">Ipsum</a></h4>
-                                    <h4><a href="#">lorem</a></h4>
-                                </div>
-                            </div>   
-                            <div class="col-sm-2">
-                                    <div class="bloque-10"> 
-                                        <h3><a href="">lorem</a></h3>
-                                        <h4><a href="#">Ipsum</a></h4>
-                                        <h4><a href="#">lorem</a></h4>
-                                    </div>
-                                </div>   
-                        </div>
-                    </div>
-        </section>
-        <section id="abajo">
-            <div class="pie-pagina">
-                <h3>AcFun</h3>
+                <div class="col-md-4">
+                     <h3 class="titulo-fo">Links</h3>
+                        <ul class="contact">
+                         <li><a href="<?=base_url();?>series"><i class="fas fa-chevron-circle-right"></i>Home </a></li>
+                         <li><a href="<?=base_url();?>login/index"><i class="fas fa-chevron-circle-right"></i>Iniciar sesion</a></li>
+                         <li><a href="#"><i class="fas fa-chevron-circle-right"></i>Audio Frecuencia</a></li>
+                         <li><a href="#"><i class="fas fa-chevron-circle-right"></i>Game center</a></li>
+                         
+                     </ul>
+                </div>
+                <div class="col-md-12 "><hr style="border-color:#868588;"></div>
+                       <div class="col-md-6">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec ligula est. Integer bibendum, </p>
             </div>
+            <div class="col-md-6" id="redes-sociales-f">
+                
+                    <ul>
+                        <li ><a  href="#"><i class="fab fa-google-plus-g fa-lg"></i></a></li>
+                        <li ><a  href="#"><i class="fab fa-facebook-f fa-lg"></i></a></li>
+                        <li ><a  href="#"><i class="fab fa-twitter fa-lg"></i></a></li>
+                        <li ><a href="#"><i class="fab fa-youtube fa-lg"  style="color: #868588 !important;"></i></a></li>
+                        <li ><a  href="#"><i class="fab fa-tumblr fa-lg"></i></a></li>
+                        <li ><a  href="#"><i class="fab fa-flickr fa-lg"></i></a></li>
+                    </ul>
+               
+            </div>
+            </div>
+            </div>
+
+            <div class="pie-pagina">
+                <div class="container">
+                    <div class="row">
+                    <p style="padding-bottom:1px;"><i class="fas fa-users fa-2x" style="padding-top: 5px; padding-left: 5px;"></i>The team</p>
+         
+        </div>
+        </div>
+            </div>          
         </section>
     </footer>
 
