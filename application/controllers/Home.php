9f74dbc424ec8e35cd4619cee9733bc63f4800f9
eJme1992 <edwin.jme@gmail.com>
2018-09-08 19:15:40 -0400

    cambios en el maquetado

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,9 +9,9 @@ class Home extends CI_Controller
         $this->load->model('MFunctionsg'); // CARGA LAS FUNCIONES GENERALES PARA EL Perfil
         $this->load->library('session'); // CARGA LAS SESSIONES
         // VERIFICA QUE EL USER ESTE LOGUEADO (La función esta dentro de Mfuntionsg)
-        $this->load->view('website/header'); //Mi carpeta y mi archivo que corresponden a la vista
-        $DATOS['user'] = $this->session->userdata('id'); // PASO LOS DATOS DEL USUARIO 
        
+        $DATOS['user'] = $this->session->userdata('id'); // PASO LOS DATOS DEL USUARIO 
+        $this->load->view('website/header',$DATOS); //Mi carpeta y mi archivo que corresponden a la vista
     }
     
     // **PAGINA DE INICIO DEL HOME**
--- application/views/website/cuerpo-home.php
+++ application/views/website/cuerpo-home.php
@@ -6,7 +6,7 @@
       <a href="#SERIES_DESTACAS"><i class="fas fa-star"></i></i> Series destacadas</a>
       <a href="#TEMPORADAS_RECIENTES"><i class="fab fa-cloudscale"></i> Temporadas </a>
       <a href="#PELICULAS_RECIENTES"><i class="fas fa-video"></i> Películas recientes</a>
-      <a href=""><i class="fas fa-video"></i> Noticias</a>
+      <a href="#NOTICIAS"><i class="fas fa-newspaper"></i> Noticias</a>
    </div>
 </nav>
 <section id="home">
