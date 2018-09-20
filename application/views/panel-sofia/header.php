a24fafcbea640f98ce4ac5acbdb97a9d6c47aa70
Sofia <sofiasinger19@gmail.com>
2018-09-20 18:54:40 -0400

    cambios

---
+++ application/views/panel-sofia/header.php
@@ -0,0 +1,86 @@
+    <!DOCTYPE html>
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
+
+        <link rel="shortcut icon" href="<?=base_url();?>plantilla/admin/images/favicon.ico">
+
+        <title>Adminto - Responsive Admin Dashboard Template</title>
+
+        <!--Morris Chart CSS -->
+        <link rel="stylesheet" href="<?=base_url();?>plantilla/admin/plugins/morris/morris.css">
+
+        <!-- App css -->
+        <link href="<?=base_url();?>plantilla/admin/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?=base_url();?>plantilla/admin/css/bootstrap.min.css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="<?=base_url();?>plantilla/admin/css/bootstrap.min.css/style.css" rel="stylesheet" type="text/css" />
+
+        <script src="<?=base_url();?>plantilla/admin/js/modernizr.min.js"></script>
+
+    </head>
+
+
+    <body class="fixed-left">
+
+        <!-- Begin page -->
+    <div class="topbar">
+
+                <!-- LOGO -->
+                <div class="topbar-left">
+                    <a href="index.html" class="logo"><span>Admin<span>to</span></span><i class="mdi mdi-layers"></i></a>
+                </div>
+
+                <!-- Button mobile view to collapse sidebar menu -->
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="container-fluid">
+
+                        <!-- Page title -->
+                        <ul class="nav navbar-nav list-inline navbar-left">
+                            <li class="list-inline-item">
+                                <button class="button-menu-mobile open-left">
+                                    <i class="mdi mdi-menu"></i>
+                                </button>
+                            </li>
+                            <li class="list-inline-item">
+                                <h4 class="page-title">Dashboard</h4>
+                            </li>
+                        </ul>
+
+                        <nav class="navbar-custom">
+
+                            <ul class="list-unstyled topbar-right-menu float-right mb-0">
+
+                                <li>
+                                    <!-- Notification -->
+                                    <div class="notification-box">
+                                        <ul class="list-inline mb-0">
+                                            <li>
+                                                <a href="javascript:void(0);" class="right-bar-toggle">
+                                                    <i class="mdi mdi-bell-outline noti-icon"></i>
+                                                </a>
+                                                <div class="noti-dot">
+                                                    <span class="dot"></span>
+                                                    <span class="pulse"></span>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <!-- End Notification bar -->
+                                </li>
+
+                                <li class="hide-phone">
+                                    <form class="app-search">
+                                        <input type="text" placeholder="Search..."
+                                               class="form-control">
+                                        <button type="submit"><i class="fa fa-search"></i></button>
+                                    </form>
+                                </li>
+
+                            </ul>
+                        </nav>
+                    </div><!-- end container -->
+                </div><!-- end navbar -->
+            </div>
