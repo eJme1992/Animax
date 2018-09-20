16bd37ae0828007bf7f104c19dd5ee3d63a8a599
eJme1992 <edwin.jme@gmail.com>
2018-09-19 20:36:11 -0400

    inicio

---
+++ application/views/website/nav.php
@@ -0,0 +1,24 @@
+<nav id="barra-secundaria">
+      <!--  <div class="navbar-header row" id="responsivenav" style="width: 100%">
+
+               <div class="col-6 offset-md-6">
+                <button class="btn" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+     <i class="fas fa-align-justify"></i>
+    </button>
+  </div>
+  </div>-->
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2" style="background-color:#fff;">
+    <div class="text-center">
+        <a href="#SERIES_DESTACAS"><i class="far fa-star"></i>SERIES</a>
+        <a href="#TEMPORADAS_RECIENTES"><i class="fab fa-cloudscale"></i>TEMPORADAS  </a>
+        <a href="#PELICULAS_RECIENTES"><i class="fas fa-film"></i>PELICULAS</a>
+        <a href="#NOTICIAS"><i class="far fa-newspaper"></i>NOTICIAS</a>
+    </div>
+</div>
+<div class="text-center" id="nav-principal">
+        <a href="#SERIES_DESTACAS"><i class="far fa-star"><span>SERIES</span></i></a>
+        <a href="#TEMPORADAS_RECIENTES"><i class="fab fa-cloudscale"><span>TEMPORADAS</span></i></a>
+        <a href="#PELICULAS_RECIENTES"><i class="fas fa-film"><span>PELICULAS</span></i></a>
+        <a href="#NOTICIAS"><i class="far fa-newspaper"><span>NOTICIAS</span></i></a>
+    </div>
+</nav>
