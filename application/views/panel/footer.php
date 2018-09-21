53b4ed39ef7bc45a0e9d5fa52f96e9db93c7449c
Sofia <sofiasinger19@gmail.com>
2018-09-20 21:06:44 -0400

    footer

--- application/views/panel/footer.php
+++ application/views/panel/footer.php
@@ -1,105 +1,123 @@
-</div>
-  <!-- /.content-wrapper -->
- <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="<?=base_url();?>plantilla/panel/#">Company</a>.</strong> All rights reserved.
-  </footer>
+<footer class="footer text-right">
+                    2016 - 2018 © Adminto. Coderthemes.com
+                </footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
-    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-      <li class="active"><a href="<?=base_url();?>plantilla/panel/#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-      <li><a href="<?=base_url();?>plantilla/panel/#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
-    <!-- Tab panes -->
-    <div class="tab-content">
-      <!-- Home tab content -->
-      <div class="tab-pane active" id="control-sidebar-home-tab">
-        <h3 class="control-sidebar-heading">Recent Activity</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="<?=base_url();?>plantilla/panel/javascript:;">
-              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+            </div>
 
-              <div class="menu-info">
-                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
 
-                <p>Will be 23 on April 24th</p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
+            <!-- ============================================================== -->
+            <!-- End Right content here -->
+            <!-- ============================================================== -->
 
-        <h3 class="control-sidebar-heading">Tasks Progress</h3>
-        <ul class="control-sidebar-menu">
-          <li>
-            <a href="<?=base_url();?>plantilla/panel/javascript:;">
-              <h4 class="control-sidebar-subheading">
-                Custom Template Design
-                <span class="pull-right-container">
-                    <span class="label label-danger pull-right">70%</span>
-                  </span>
-              </h4>
 
-              <div class="progress progress-xxs">
-                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <!-- /.control-sidebar-menu -->
+            <!-- Right Sidebar -->
+            <div class="side-bar right-bar">
+                <a href="javascript:void(0);" class="right-bar-toggle">
+                    <i class="mdi mdi-close-circle-outline"></i>
+                </a>
+                <h4 class="">Notifications</h4>
+                <div class="notification-list nicescroll">
+                    <ul class="list-group list-no-border user-list">
+                        <li class="list-group-item">
+                            <a href="#" class="user-list-item">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>plantilla/admin/images/users/avatar-2.jpg" alt="">
+                                </div>
+                                <div class="user-desc">
+                                    <span class="name">Michael Zenaty</span>
+                                    <span class="desc">There are new settings available</span>
+                                    <span class="time">2 hours ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="user-list-item">
+                                <div class="icon bg-info">
+                                    <i class="mdi mdi-account"></i>
+                                </div>
+                                <div class="user-desc">
+                                    <span class="name">New Signup</span>
+                                    <span class="desc">There are new settings available</span>
+                                    <span class="time">5 hours ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="#" class="user-list-item">
+                                <div class="icon bg-pink">
+                                    <i class="mdi mdi-comment"></i>
+                                </div>
+                                <div class="user-desc">
+                                    <span class="name">New Message received</span>
+                                    <span class="desc">There are new settings available</span>
+                                    <span class="time">1 day ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item active">
+                            <a href="#" class="user-list-item">
+                                <div class="avatar">
+                                    <img src="<?=base_url();?>plantilla/admin/images/users/avatar-3.jpg" alt="">
+                                </div>
+                                <div class="user-desc">
+                                    <span class="name">James Anderson</span>
+                                    <span class="desc">There are new settings available</span>
+                                    <span class="time">2 days ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="list-group-item active">
+                            <a href="#" class="user-list-item">
+                                <div class="icon bg-warning">
+                                    <i class="mdi mdi-settings"></i>
+                                </div>
+                                <div class="user-desc">
+                                    <span class="name">Settings</span>
+                                    <span class="desc">There are new settings available</span>
+                                    <span class="time">1 day ago</span>
+                                </div>
+                            </a>
+                        </li>
 
-      </div>
-      <!-- /.tab-pane -->
-      <!-- Stats tab content -->
-      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-      <!-- /.tab-pane -->
-      <!-- Settings tab content -->
-      <div class="tab-pane" id="control-sidebar-settings-tab">
-        <form method="post">
-          <h3 class="control-sidebar-heading">General Settings</h3>
+                    </ul>
+                </div>
+            </div>
+            <!-- /Right-bar -->
 
-          <div class="form-group">
-            <label class="control-sidebar-subheading">
-              Report panel usage
-              <input type="checkbox" class="pull-right" checked>
-            </label>
+        </div>
+        <!-- END wrapper -->
 
-            <p>
-              Some information about this general settings option
-            </p>
-          </div>
-          <!-- /.form-group -->
-        </form>
-      </div>
-      <!-- /.tab-pane -->
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-  <!-- Add the sidebar's background. This div must be placed
-  immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
 
-<!-- REQUIRED JS SCRIPTS -->
+        <!-- jQuery  -->
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.min.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/popper.min.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/bootstrap.min.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/detect.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/fastclick.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.blockUI.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/waves.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.nicescroll.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.slimscroll.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.scrollTo.min.js"></script>
 
-<!-- jQuery 3 -->
-<script src="<?=base_url();?>plantilla/panel/bower_components/jquery/dist/jquery.min.js"></script>
-<!-- Bootstrap 3.3.7 -->
-<script src="<?=base_url();?>plantilla/panel/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- AdminLTE App -->
-<script src="<?=base_url();?>plantilla/panel/dist/js/adminlte.min.js"></script>
+        <!-- KNOB JS -->
+        <!--[if IE]>
+        <script type="text/javascript" src="<?=base_url();?>plantilla/admin/plugins/jquery-knob/excanvas.js"></script>
+        <![endif]-->
+        <script src="<?=base_url();?>plantilla/admin/plugins/jquery-knob/jquery.knob.js"></script>
 
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-     Both of these plugins are recommended to enhance the
-     user experience. -->
-</body>
+        <!--Morris Chart-->
+        <script src="<?=base_url();?>plantilla/admin/plugins/morris/morris.min.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/plugins/raphael/raphael-min.js"></script>
+
+        <!-- Dashboard init -->
+        <script src="<?=base_url();?>plantilla/admin/pages/jquery.dashboard.js"></script>
+
+        <!-- App js -->
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.core.js"></script>
+        <script src="<?=base_url();?>plantilla/admin/js/jquery.app.js"></script>
+
+    </body>
+
+<!-- Mirrored from coderthemes.com/adminto/dark_menu/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 07 Sep 2018 18:36:45 GMT -->
 </html>
